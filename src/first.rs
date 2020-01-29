c3f04f7d7c267277284e22dbefb4e1233b86b75f
bbc <bbc@163.com>
2020-01-29 10:33:16 +0800

    Enter

---
+++ src/first.rs
@@ -0,0 +1,46 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+            }
+            Link::More(ref node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            }
+        };
+        result
+    }
+}
+
+
+
